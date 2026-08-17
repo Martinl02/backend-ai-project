3701a5bda73518db7b90c7b6fb4a099b10f517d3
Martín Luraschi <martinl02@gmail.com>
2026-08-17 03:48:35 -0300

    Integrar Pandas para análisis de datos

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Proyecto Backend + IA")
+import requests
+
+url = "https://jsonplaceholder.typicode.com/users"
+
+try:
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(len(data))
+
+    elif response.status_code == 404:
+        print("Endpoint no encontrado (404)")
+
+    else:
+        print(f"Error inesperado: {response.status_code}")
+
+except Exception as e:
+    print(f"Error de conexión: {e}")
